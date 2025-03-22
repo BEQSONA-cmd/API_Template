26a82d255093f5dff296bbb863f9449052366bb3
Beqa <tvildiani2001@gmail.com>
2025-03-22 14:38:23 +0100

    ad

---
+++ server.ts
@@ -0,0 +1,44 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import cookie from "@fastify/cookie";
+import cron from "node-cron";
+import registerRoutes from "./api/register";
+import loginRoutes from "./api/login";
+import meRoutes from "./api/me";
+import fs from "fs";
+
+const fastify = Fastify({
+    https: {
+        key: fs.readFileSync("./ssl/key.pem"),
+        cert: fs.readFileSync("./ssl/cert.pem"),
+    },
+});
+    
+fastify.register(cors, {
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+});
+
+fastify.register(cookie);
+fastify.register(registerRoutes);
+fastify.register(loginRoutes);
+fastify.register(meRoutes);
+
+cron.schedule("*/30 * * * * *", async () => {
+    console.log("🔄 Checking something...");
+});
+
+const startServer = async () => {
+    try {
+        await fastify.listen({ port: 8080, host: "0.0.0.0" });
+        console.log("Fastify server is running on https://localhost:8080");
+    } catch (err) {
+        fastify.log.error(err);
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+startServer();
